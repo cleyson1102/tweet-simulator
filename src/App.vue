8e9faf772b3a35f63d11b1a68e5de7ecd2d51d82
Cleyson N <Cleyson.nh@gmail.com>
2022-05-06 09:45:27 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <Menu :openCloseForm="openCloseForm" :showForm="showForm" />
+  <TweetForm
+    :showForm="showForm"
+    :openCloseForm="openCloseForm"
+    :reloadTweets="reloadTweets"
+  />
+  <TweetList :tweets="tweets" :reloadTweets="reloadTweets" />
+</template>
+
+<script>
+import { ref } from "vue";
+import Menu from "@/components/Menu.vue";
+import TweetForm from "@/components/TweetForm.vue";
+import useFormTweet from "@/hooks/UserFormTweet";
+import TweetList from "@/components/TweetList.vue";
+import { getTweetApi } from "@/api/Tweet";
+
+export default {
+  name: "App",
+  components: {
+    Menu,
+    TweetForm,
+    TweetList,
+  },
+
+  setup() {
+    let tweets = ref(getTweetApi().reverse());
+
+    const reloadTweets = () => {
+      tweets.value = getTweetApi().reverse();
+    };
+    return {
+      ...useFormTweet(),
+      tweets,
+      reloadTweets,
+    };
+  },
+};
+</script>
+
+<style lang="scss"></style>
---
+++ src/components/Menu.vue
@@ -0,0 +1,30 @@
+<template>
+  <nav class="navbar navbar-dark bg-dark">
+    <div class="container">
+      <span class="navbar-brand">Simulador de Tweets</span>
+
+      <button
+        class="btn"
+        :class="{ 'btn-success': !showForm, 'btn-danger': showForm }"
+        @click="openCloseForm"
+      >
+        {{ !showForm ? "Nuevo Tweet" : "Cerrar Tweet" }}
+      </button>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  props: {
+    showForm: Boolean,
+    openCloseForm: Function,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+button {
+  margin-right: auto;
+}
+</style>
